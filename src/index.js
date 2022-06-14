a62c5e2008a49f38f05697999df27abb960fd0b1
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-06-15 00:34:32 +0300

    fetch on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+  const inputForm=document.querySelector('form');
+
+  inputForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+   // Access Input Value Directly
+   const input = document.querySelector('input#searchByID');
+    /*accesing input value from an event object
+    //e.target.children[1].value*/   
+    //console.log(input.value);
+
+    //Fetch Data Based on User Input
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response =>response.json())
+    .then(data =>{
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+        //console.log(data);
+    });
+} );
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
